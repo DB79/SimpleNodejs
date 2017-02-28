0928fef7c49c6b181dbc256c21737896fc7d77e5
Damien Breen <damienbreen7@gmail.com>
2017-02-28 17:18:45 +0000

    added done function to try to try and get process to exit after tests have completed

--- test/tests.js
+++ test/tests.js
@@ -4,18 +4,25 @@ const func = require("../public/js/functions");
 var expect = chai.expect;
 var should = chai.should();
 
-it('1 should equal 1', function() {
-  expect(1).to.equal(1);
+describe('add a entry into the index', function () {
+  it('1 should equal 1', function (done) {
+    expect(1).to.equal(1);
+    done();
+  });
+  it('Should always return a boolean', function (done) {
+    expect(func.isEven(2)).to.be.a('boolean');
+    done();
+  });
+
+  it('Should always return a boolean', function (done) {
+    expect(func.isEven(3)).to.be.a('boolean');
+    done();
+  });
+  it('2+2 should be equal to 4', function (done) {
+    expect(func.addNumbers(2, 2)).to.equal(4);
+    done();
+  });
 });
 
-it('Should always return a boolean', function() {
-  expect(func.isEven(2)).to.be.a('boolean');
-});
 
-it('Should always return a boolean', function() {
-  expect(func.isEven(3)).to.be.a('boolean');
-});
 
-it('2+2 should be equal to 4', function() {
-  expect(func.addNumbers(2,2)).to.equal(4);
-});
